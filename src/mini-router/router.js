9afc60cc7d389ab047ebf62d42bf4f0fbd22546d
miaokeliq <921418076@qq.com>
2023-04-09 09:54:15 +0800

    监听导航事件并相应变化

--- src/mini-router/router.js
+++ src/mini-router/router.js
@@ -1,9 +1,11 @@
 import RouterLink from "./RouterLink";
 import RouterView from "./RouterView";
-
+import { ref } from "vue";
 export function createRouter(options) {
   // router 实例
   const router = {
+    options, // 保存配置项
+    current: ref(window.location.hash.slice(1) || "/"),
     // install 调用者不确定
     install(app) {
       const router = this;
@@ -16,6 +18,13 @@ export function createRouter(options) {
       app.config.globalProperties.$router = router;
     },
   };
+
+  // 监听事件
+  window.addEventListener("hashchange", () => {
+    // 变化保存到 current 并触发 RouterView 更新
+    router.current.value = window.location.hash.slice(1);
+    console.log(router.current.value);
+  });
 
   return router;
 }
